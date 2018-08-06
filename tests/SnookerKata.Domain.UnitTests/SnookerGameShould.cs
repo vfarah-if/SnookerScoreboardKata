dfa0e64018d9ae0872273ac075579163c82637ba
Vincent Farah <vincent.farah@codurance.com>
2018-08-07 00:13:08 +0100

    Created an empty project ready to start playing

---
+++ tests/SnookerKata.Domain.UnitTests/SnookerGameShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Xunit;
+
+namespace SnookerKata.Domain.UnitTests
+{
+    public class SnookerGameShould
+    {
+        private readonly Fixture autoFixture;
+
+        public SnookerGameShould()
+        {
+            autoFixture = new Fixture();
+            autoFixture.Customize(new AutoMoqCustomization());
+        }
+
+        [Fact]
+        public void ShouldHave()
+        {
+            autoFixture.Create<bool>().Should().BeTrue();
+        }
+
+    }
+}
